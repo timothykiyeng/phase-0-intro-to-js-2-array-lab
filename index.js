0d3819f31a9b75284a6ff965fffcdc685c8e18df
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-05 19:27:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+
+function appendCat(name) {
+  appendCat = [...cats, "Broom"];
+  return appendCat;
+}
+
+function prependCat(name) {
+  appendCat = ["Arnold", ...cats];
+  return appendCat;
+}
+
+function removeLastCat() {
+  removeLastCat = cats.slice(0, -1);
+  return removeLastCat;
+}
+
+function removeFirstCat() {
+  removeFirstCat = cats.slice(1);
+  return removeFirstCat;
+}
